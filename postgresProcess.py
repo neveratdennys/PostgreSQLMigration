4afd6749c8e2358da5c9b56bda73494bc9b94999
neveratdennys <48944016+neveratdennys@users.noreply.github.com>
2022-07-21 10:22:45 -0700

    add schema name (dbo.) and migrate convert function
    a set schema name is added to all FROM clauses, and convert functions that act as type casts are changed from convert(A,B) to B::A.

---
+++ postgresProcess.py
@@ -0,0 +1,137 @@
+import sys
+import pip
+import glob
+import re
+
+
+# Check for balanced brackets
+def checkBracket(my_string):
+    count = 0
+    for c in my_string:
+        if c == "(":
+            count+=1
+        elif c == ")":
+            count-=1
+    return count
+
+
+# Modify the first convert in line
+def modifyConvert(l):
+    # find the location of convert()
+    count = l.index('convert(')
+
+    # select the group before convert() call
+    before = l[:count]
+
+    group=""
+    n1=0
+    n2=0
+    A=""
+    B=""
+    operate = False
+    # look for A group before comma
+    for n1, i in enumerate(l[count+8:], start=len(before)+8):
+        # find current position in l
+        checkIndex = checkBracket(l[count+8:][:n1-len(before)-8])
+        if i == ',' and checkIndex == 0:
+            A = group
+            break
+        group += i
+
+    # look for B group after comma
+    group = ""
+    for n2, i in enumerate(l[n1+1:], start=n1+1):
+        checkIndex = checkBracket(l[count+8+n1-len(before)-8:][:n2-n1-1])
+        if i == ',' and checkIndex == 0:
+            return l
+        elif checkIndex < 0:
+            B = group
+            break
+        group += i
+        
+        # mark operators
+        if i == '|':
+            operate = True
+
+    # select the group after convert() call
+    after = l[n2:]
+
+    # (B) if it contains operators
+    if operate:
+        return before + "(" + B.lstrip().rstrip(") ") + ') :: ' + A + " " + after
+    else:
+        return before + B.lstrip().rstrip(") ") + '::' + A + " " + after
+
+
+# Modify cast syntax with convert(a,b). return line.
+def convertCast(l):
+
+    # Call helper for nested cases
+    i = l.count('convert(')
+    while i>0:
+        i -= 1
+        l = modifyConvert(l)
+
+    return l
+
+
+
+#input file
+fin = open("testviews.sql", "rt")
+#output file to write the result to
+fout = open("testout.sql", "wt")
+
+# marker for from
+fromMarker = 0
+
+# for each line in the input file
+for line in fin:
+
+    # replace non leading tabs with space
+    while re.findall(r'[A-Za-z0-9]\t', line):
+        line = re.sub(r'([A-Za-z0-9])\t', r'\1 ', line)
+
+    # remove duplicate spaces
+    space = 1
+    while '  ' in line:
+        space += 1
+        if space > 3:
+            line = line.replace('  ', '\t')
+            space = 1
+        else:
+            line = line.replace('  ', ' ')
+
+    # Next line if line is None
+    if not line:
+        continue
+
+
+    # Replace convert(A, B) for B::A
+    if ("convert(" in line):
+        line = convertCast(line)
+
+
+    # Add dbo. if line starts with FROM and add mark for next line
+    matchFrom = ["from ", "FROM ", "From "]
+    matchJoin = ["join ", "JOIN ", "Join "]
+    matchWhere = ["where ", "WHERE ", "Where "]
+    x = next((x for x in matchFrom if x in line), False)
+    y = next((y for y in matchJoin if y in line), False)
+    z = next((z for z in matchWhere if z in line), False)
+    if x:
+        #read replace the string and write to output file
+        fout.write(re.sub(re.escape(x), 'FROM ' + 'dbo.', line))
+        fromMarker += 1
+    # Find the join statements and add dbo.
+    elif fromMarker and y:
+        fout.write(re.sub(re.escape(y), 'JOIN ' + 'dbo.', line))
+    # Find where statements and capitalize and reset marker
+    elif fromMarker and z:
+        fout.write(re.sub(re.escape(z), "WHERE ", line))
+        fromMarker -= 1
+    else:
+        fout.write(line)
+
+#close input and output files
+fin.close()
+fout.close()
